6b317e4e33d045cb6979428c60be7937cffbcfc2
hyhy0623 <rlagkdus0623@naver.com>
2024-01-20 23:11:35 +0900

    feat: Add learn fragment detail page (#1)

--- Android/app/src/main/java/com/example/pro_diction/LearnFragment.kt
+++ Android/app/src/main/java/com/example/pro_diction/LearnFragment.kt
@@ -1,10 +1,12 @@
 package com.example.pro_diction
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class LearnFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learn, container, false)
+        val learnInflater = inflater.inflate(R.layout.fragment_learn, container, false)
+        learnInflater.findViewById<Button>(R.id.btn_test).setOnClickListener {
+            val intent = Intent(requireContext(), LearnDetailActivity::class.java)
+            startActivity(intent)
+        }
+        return learnInflater
     }
 
     companion object {
