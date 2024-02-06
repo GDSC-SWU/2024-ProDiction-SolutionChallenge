69f1c0633f4d9488a3c74f0ab47892d610e84f57
hyhy0623 <rlagkdus0623@naver.com>
2024-02-07 07:38:59 +0900

    feat: Add call function (#1)
    전화 걸기 기능 추가, permission 설정

--- Android/app/src/main/java/com/example/pro_diction/presentation/my/CallActivity.kt
+++ Android/app/src/main/java/com/example/pro_diction/presentation/my/CallActivity.kt
@@ -1,10 +1,15 @@
 package com.example.pro_diction.presentation.my
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.pro_diction.R
 import com.example.pro_diction.presentation.learn.SearchActivity
 
@@ -19,6 +24,22 @@ class CallActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        // call 전화 걸기
+        val btnCall: Button = findViewById(R.id.btn_call_now)
+        btnCall.setOnClickListener {
+            var intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:107")
+
+            if (ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.CALL_PHONE), 0)
+
+                return@setOnClickListener
+            }
+            startActivity(intent)
+
+        }
     }
 
     // 툴바 메뉴 클릭 됐을 때
