62bf503d621a3396171e03140fc437984ebe7053
hyhy0623 <rlagkdus0623@naver.com>
2024-02-08 12:54:18 +0900

    feat: Add stt in comm fragment (#)
    안드로이드에서 제공하는 stt 적용 (comm 프래그먼트)

--- Android/app/src/main/java/com/example/pro_diction/presentation/comm/CommFragment.kt
+++ Android/app/src/main/java/com/example/pro_diction/presentation/comm/CommFragment.kt
@@ -16,14 +16,19 @@
 package com.example.pro_diction.presentation.comm
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsClient.getPackageName
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -71,7 +76,16 @@ class CommFragment : Fragment(),
     // permissions
     private val REQUIRED_PERMISSIONS = mutableListOf(android.Manifest.permission.INTERNET,
         android.Manifest.permission.CAMERA,
-        android.Manifest.permission.ACCESS_NETWORK_STATE).toTypedArray()
+        android.Manifest.permission.ACCESS_NETWORK_STATE, android.Manifest.permission.RECORD_AUDIO).toTypedArray()
+
+    // stt
+    lateinit var speechRecognizer: SpeechRecognizer
+    var recodeStr: String = ""
+        set(value) {
+            field = value
+            // TextView에 변경된 recodeStr 값을 설정
+            fragmentCommBinding.tvStt.text = value
+        }
 
     override fun onResume() {
         super.onResume()
@@ -147,9 +161,10 @@ class CommFragment : Fragment(),
             .check()
 
         fragmentCommBinding.btn.setOnClickListener {
-            Log.e("클릭", "클릭")
-            Toast.makeText(requireContext(), "클릭", Toast.LENGTH_SHORT).show()
+            startVoiceRecording()
+            fragmentCommBinding.tvStt.text = recodeStr
         }
+        fragmentCommBinding.tvStt.text = recodeStr
 
         return fragmentCommBinding.root
     }
@@ -428,6 +443,64 @@ class CommFragment : Fragment(),
                     GestureRecognizerHelper.DELEGATE_CPU, false
                 )
             }
+        }
+    }
+    // android stt SpeechRecognizer
+    private fun startVoiceRecording() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getActivity()?.getPackageName())
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireContext())
+        speechRecognizer.setRecognitionListener(recognitionListener)
+        speechRecognizer.startListening(intent)
+    }
+
+    private val recognitionListener: RecognitionListener = object : RecognitionListener {
+        override fun onReadyForSpeech(params: Bundle?) {
+            Toast.makeText(requireContext(), "음성인식 시작", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onBeginningOfSpeech() {
+        }
+
+        override fun onRmsChanged(rmsdB: Float) {
+        }
+
+        override fun onBufferReceived(buffer: ByteArray?) {
+        }
+
+        override fun onEndOfSpeech() {
+        }
+
+        override fun onError(error: Int) {
+            val message = when (error) {
+                SpeechRecognizer.ERROR_AUDIO -> "오디오 에러"
+                SpeechRecognizer.ERROR_CLIENT -> "클라이언트 에러"
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "퍼미션 없음"
+                SpeechRecognizer.ERROR_NETWORK -> "네트워크 에러"
+                SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "네트웍 타임아웃"
+                SpeechRecognizer.ERROR_NO_MATCH -> "찾을 수 없음"
+                SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RECOGNIZER 가 바쁨"
+                SpeechRecognizer.ERROR_SERVER -> "서버가 이상함"
+                SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "말하는 시간초과"
+                else -> "알 수 없는 오류임"
+            }
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onResults(results: Bundle?) {
+            val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+            recodeStr = "" // 음성인식을 두번 연속 할 때 기존 인식된 텍스트를 초기화 하는 코드
+            for (i in matches!!.indices) recodeStr += matches[i]
+            Log.d("TEXT", "$recodeStr")
+        }
+
+        override fun onPartialResults(partialResults: Bundle?) {
+        }
+
+        override fun onEvent(eventType: Int, params: Bundle?) {
         }
     }
 }
