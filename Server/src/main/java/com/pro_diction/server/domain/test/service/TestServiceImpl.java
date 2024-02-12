bcb4ace2e67b06a61ea7d227106a4045e21139c9
suucong <sophia021210@naver.com>
2024-02-12 16:08:09 +0900

    feat: Add restTemplate timeout (#12)

--- Server/src/main/java/com/pro_diction/server/domain/test/service/TestServiceImpl.java
+++ Server/src/main/java/com/pro_diction/server/domain/test/service/TestServiceImpl.java
@@ -16,6 +16,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
@@ -57,7 +59,6 @@ public class TestServiceImpl implements TestService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public TestResponseDto testDiction(MultipartFile multipartFile, Long id) throws IOException {
         Study study = studyRepository.findById(id).orElseThrow(() -> new StudyNotFoundException());
         Double score = test(multipartFile, study.getPronunciation());
@@ -81,7 +82,10 @@ public class TestServiceImpl implements TestService {
     }
 
     private MultipartFile removeNoise(MultipartFile file) {
-        RestTemplate restTemplate = new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(2000);
+        RestTemplate restTemplate = new RestTemplate(factory);
 
         // HTTP 헤더 설정
         HttpHeaders headers = new HttpHeaders();
@@ -93,24 +97,20 @@ public class TestServiceImpl implements TestService {
 
         // Request Header, Body 요청 구성
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+        ResponseEntity<MultipartFile> responseEntity = restTemplate.postForEntity(PRODICTION_AI_API_URL + "/noisereduce", requestEntity, MultipartFile.class);
 
-        return restTemplate.postForEntity(PRODICTION_AI_API_URL + "/noisereduce", requestEntity, MultipartFile.class)
-                .getBody();
+        return responseEntity.getBody();
     }
 
     private Double test(MultipartFile file, String pronunciation) throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(2000);
+        RestTemplate restTemplate = new RestTemplate(factory);
         HttpHeaders httpHeaders = new HttpHeaders();
         String audioContents = null;
         Map<String, Object> request = new HashMap<>();
         Map<String, String> argument = new HashMap<>();
-
-        // .wav 음성 파일 노이즈 제거
-        MultipartFile removedNoiseFile = removeNoise(file);
-
-        if(removedNoiseFile != null) {
-            file = removedNoiseFile;
-        }
 
         // .wav 음성 파일을 Base64로 인코딩
         byte[] audioBytes = file.getBytes();
