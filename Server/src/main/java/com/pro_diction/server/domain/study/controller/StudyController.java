6cdd6692fba1cecc344b2da085e1c3f912ddc9c6
suucong <sophia021210@naver.com>
2024-02-15 01:19:35 +0900

    refactor: Remove unused requestDto (#17)

--- Server/src/main/java/com/pro_diction/server/domain/study/controller/StudyController.java
+++ Server/src/main/java/com/pro_diction/server/domain/study/controller/StudyController.java
@@ -35,16 +35,16 @@ public class StudyController {
         return ApiDataResponseDto.of(studyService.getStudyList(subCategoryId,parentStudyId));
     }
 
-    @GetMapping("/{id}")
-    public ApiDataResponseDto<DetailStudyResponseDto> getDetailStudy(@PathVariable @NotNull Long id) {
+    @GetMapping("/{studyId}")
+    public ApiDataResponseDto<DetailStudyResponseDto> getDetailStudy(@PathVariable @NotNull Long studyId) {
 
-        return ApiDataResponseDto.of(studyService.getDetailStudy(id));
+        return ApiDataResponseDto.of(studyService.getDetailStudy(studyId));
     }
 
-    @PostMapping
-    public ApiDataResponseDto<StudyResultDto> getStudyResult(@RequestPart @NotNull MultipartFile multipartFile,
-                                                             @RequestParam @NotNull Long id) throws IOException {
+    @PostMapping("/{studyId}")
+    public ApiDataResponseDto<StudyResultDto> getStudyResult(@PathVariable @NotNull Long studyId,
+                                                             @RequestPart @NotNull MultipartFile multipartFile) throws IOException {
 
-        return ApiDataResponseDto.of(studyService.getStudyResult(multipartFile, id));
+        return ApiDataResponseDto.of(studyService.getStudyResult(multipartFile, studyId));
     }
 }
