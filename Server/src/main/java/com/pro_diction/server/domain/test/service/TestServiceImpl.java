c460cdebb336a1656b2708d21cf4bc384852ed97
suucong <sophia021210@naver.com>
2024-02-14 16:10:06 +0900

    feat: Extract test method into DictionTestUtil (#12)

--- Server/src/main/java/com/pro_diction/server/domain/test/service/TestServiceImpl.java
+++ Server/src/main/java/com/pro_diction/server/domain/test/service/TestServiceImpl.java
@@ -9,14 +9,10 @@ import com.pro_diction.server.domain.test.dto.*;
 import com.pro_diction.server.domain.test.entity.Test;
 import com.pro_diction.server.domain.test.exception.InvalidStageException;
 import com.pro_diction.server.domain.test.repository.TestRepository;
+import com.pro_diction.server.global.util.DictionTestUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -28,12 +24,7 @@ import java.util.stream.Collectors;
 public class TestServiceImpl implements TestService {
     private final StudyRepository studyRepository;
     private final TestRepository testRepository;
-
-    @Value("${DICTION_TEST_API_URL}")
-    private String DICTION_TEST_API_URL;
-
-    @Value("${DICTION_TEST_API_KEY}")
-    private String DICTION_TEST_API_KEY;
+    private final DictionTestUtil dictionTestUtil;
 
     @Override
     @Transactional(readOnly = true)
@@ -51,8 +42,8 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public TestResponseDto testDiction(MultipartFile multipartFile, Long id) throws IOException {
-        Study study = studyRepository.findById(id).orElseThrow(() -> new StudyNotFoundException());
-        Double score = test(multipartFile, study.getPronunciation());
+        Study study = studyRepository.findById(id).orElseThrow(StudyNotFoundException::new);
+        Double score = dictionTestUtil.test(multipartFile, study.getPronunciation());
 
         return TestResponseDto.builder()
                 .studyId(id)
@@ -70,40 +61,5 @@ public class TestServiceImpl implements TestService {
         testRepository.save(test);
 
         return stage;
-    }
-
-    private Double test(MultipartFile file, String pronunciation) throws IOException {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(2000);
-        factory.setReadTimeout(2000);
-        RestTemplate restTemplate = new RestTemplate(factory);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        String audioContents = null;
-        Map<String, Object> request = new HashMap<>();
-        Map<String, String> argument = new HashMap<>();
-
-        // .wav 음성 파일을 Base64로 인코딩
-        byte[] audioBytes = file.getBytes();
-        audioContents = Base64.getEncoder().encodeToString(audioBytes);
-
-        // Request Header 구성
-        httpHeaders.set("Authorization", DICTION_TEST_API_KEY);
-
-        // Request Body 구성
-        argument.put("language_code", "korean");
-        argument.put("script", pronunciation);
-        argument.put("audio", audioContents);
-        request.put("argument", argument);
-
-        // Request Header, Body 요청 구성
-        HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(request, httpHeaders);
-
-        TestApiResponseDto testApiResponseDto =
-                restTemplate.postForEntity(DICTION_TEST_API_URL, httpEntity, TestApiResponseDto.class).getBody();
-        try {
-            return Math.round(Double.valueOf(testApiResponseDto.getReturn_object().getScore()) / 5 * 100 * 10) / 10.0;
-        } catch (Exception e) {
-            return 0.0;
-        }
     }
 }
