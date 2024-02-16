ca37fad94dbab10c6ff5a1d890035754f0924db7
suucong <sophia021210@naver.com>
2024-02-16 16:05:08 +0900

    feat: Modify request format for diction test API (#35)

--- Server/src/main/java/com/pro_diction/server/domain/test/controller/TestController.java
+++ Server/src/main/java/com/pro_diction/server/domain/test/controller/TestController.java
@@ -5,6 +5,7 @@ import com.pro_diction.server.domain.test.dto.TestContentResponseDto;
 import com.pro_diction.server.domain.test.dto.TestResponseDto;
 import com.pro_diction.server.domain.test.service.TestService;
 import com.pro_diction.server.global.common.ApiDataResponseDto;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,11 +29,11 @@ public class TestController {
         return ApiDataResponseDto.of(testService.getTestStudyList(stage));
     }
 
-    @PostMapping("/diction")
-    public ApiDataResponseDto<TestResponseDto> test(@RequestPart MultipartFile multipartFile,
-                                                    @RequestParam Long id) throws IOException {
+    @PostMapping("/diction/{studyId}")
+    public ApiDataResponseDto<TestResponseDto> test(@RequestPart @NotNull MultipartFile multipartFile,
+                                                    @PathVariable @NotNull Long studyId) throws IOException {
 
-        return ApiDataResponseDto.of(testService.testDiction(multipartFile, id));
+        return ApiDataResponseDto.of(testService.testDiction(multipartFile, studyId));
     }
 
     @PostMapping
