e3d1210fe837254e40b4a47d0bb18ced179ceb46
suucong <sophia021210@naver.com>
2024-02-23 15:35:54 +0900

    feat: Add  to unnecessary search delete API (#29)

--- Server/src/main/java/com/pro_diction/server/domain/search/service/SearchServiceImpl.java
+++ Server/src/main/java/com/pro_diction/server/domain/search/service/SearchServiceImpl.java
@@ -78,6 +78,7 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     @Async
+    @Transactional
     @Scheduled(cron = "0 0 4 ? * MON", zone = "Asia/Seoul")
     public void cleanSearchHistory() {     // 스케줄링을 통해 필요없는 검색 기록 삭제(매주 월요일 오전 4시)
         List<Member> memberList = memberRepository.findAll();
